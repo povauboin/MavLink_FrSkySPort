197847372f94a84f533dd7a44329d82d3f065322
wolkstein <m.wolkstein@gmx.de>
2015-09-01 03:28:09 +0200

    remove unused things

--- MavLink_FrSkySPort/LSCM.cpp
+++ MavLink_FrSkySPort/LSCM.cpp
@@ -8,34 +8,6 @@
  * detailed informations and schematics here
  *
  */
- 
-/*
-   //cell voltage divider. this is dependent from your resitor voltage divider network
-  double LIPOCELL_1TO8[13] =
-  {
-    1905.331599479, // 277.721518987, //1897.85344189,// 10bit 237.350026082,   3,97
-    929.011553273,  // 137.358490566, //926.799312208,// 10bit 116.006256517,   8,03
-    615.667808219,  // 91.373534338,  //618.198183455,// 10bit 77.3509473318,  12,04
-    0.0, // 470.134166514,// 10bit 58.7966886122,
-    0.0, // 370.317778975,// 10bit 46.3358699051,
-    0.0, // 315.045617465,// 10bit 39.4176445024,
-    0.0, // diverders 7-12 not defined because my network includes only 6 voltage dividers
-    0.0,
-    0.0,
-    0.0,
-    0.0,
-    0.0
-  };
-  
-  double individualcelldivider[MAXCELLS+1];
-  uint8_t analogread_threshold = 100;         // threshold for connected zelldetection in mV
-  uint8_t cells_in_use = MAXCELLS;
-  int32_t zelle[MAXCELLS+1];
-  double cell[MAXCELLS+1];
-  int32_t alllipocells = 0;
-  float lp_filter_val = 0.99; // this determines smoothness  - .0001 is max  0.99 is off (no smoothing)
-  double smoothedVal[MAXCELLS+1]; // this holds the last loop value
-  */
 
 #include "LSCM.h"
 
