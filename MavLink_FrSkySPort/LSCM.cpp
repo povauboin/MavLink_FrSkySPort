970ed5040d4dddeebef99e1f1647a884226da854
wolkstein <m.wolkstein@gmx.de>
2015-09-20 20:59:44 +0200

    add gpl3 license to LSCM class

--- MavLink_FrSkySPort/LSCM.cpp
+++ MavLink_FrSkySPort/LSCM.cpp
@@ -1,4 +1,27 @@
-/* 
+/**
+ *
+ * Class LSCM, part of MavLink_FrSkySPort
+ * 
+ * Copyright (C) 2015 Michael Wolkstein
+ * https://github.com/Clooney82/MavLink_FrSkySPort
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *
+ *
+ * 
  * Class of Wolke lipo-single-cell-monitor
  * *******************************************
  * This will monitor 1 - 8 cells of your lipo.
