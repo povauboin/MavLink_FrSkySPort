52470230f69d51390589eaa813ed40feb5948140
wolkstein <m.wolkstein@gmx.de>
2015-09-20 20:44:21 +0200

    add license infos to lua mixer scripts

--- Lua_Telemetry/DisplayApmPosition/SCRIPTS/MIXES/cellinfo.lua
+++ Lua_Telemetry/DisplayApmPosition/SCRIPTS/MIXES/cellinfo.lua
@@ -1,3 +1,24 @@
+--
+-- CellInfo lua
+--
+-- Copyright (C) 2014 Michael Wolkstein
+-- 
+-- https://github.com/Clooney82/MavLink_FrSkySPort
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY, without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
 
 local inputs = { {"Crit,V/100", VALUE,300,350,340}, {"Use Horn", VALUE, 0, 3, 0}, {"Warn,V/100", VALUE, 310, 380, 350}, {"Rep, Sec", VALUE, 3, 30, 4},{"Drop, mV", VALUE, 1, 500, 100} }
 
--- Lua_Telemetry/DisplayApmPosition/SCRIPTS/MIXES/offset.lua
+++ Lua_Telemetry/DisplayApmPosition/SCRIPTS/MIXES/offset.lua
@@ -1,3 +1,25 @@
+--
+-- Offset lua
+--
+-- Copyright (C) 2014 Michael Wolkstein
+-- 
+-- https://github.com/Clooney82/MavLink_FrSkySPort
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY, without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+
 local inputs = { {"O-SET mAh% ", VALUE,-100,100,0}, {"O-SET Wh%", VALUE, -100, 100, 0}, {"BatCap Wh", VALUE, 0, 250, 30} }
 
 local oldoffsetmah=0
